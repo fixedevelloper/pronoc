89c174fcb4da871aa17bbadf8da58323bda9f020
fixedevelloper <fixedevelloper@gmail.com>
2025-11-19 08:59:02 +0100

    deb

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from "next";
 import HomePage from "./components/HomePage";
 
-export const metadata: Metadata = { title: 'Accueil-Skypay' }
+export const metadata: Metadata = { title: 'Accueil-pronocrew' }
 export default function Home() {
 
   return (
